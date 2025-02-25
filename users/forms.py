ef3da43e6a7a3a7f122bf8a3b20b4124209723aa
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2025-02-25 22:08:50 +0000

    Feat: Update user form , add edit profile view.

---
+++ users/forms.py
@@ -0,0 +1,30 @@
+# users/forms.py
+from django import forms
+from .models import CustomUser
+
+class CustomUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = [
+            # Core fields
+            'first_name', 'last_name', 'email',
+            # Shipping
+            'shipping_full_name', 'shipping_phone', 'shipping_street_address1',
+            'shipping_street_address2', 'shipping_city', 'shipping_postcode',
+            'shipping_country',
+            # Billing
+            'billing_full_name', 'billing_phone', 'billing_street_address1',
+            'billing_street_address2', 'billing_city', 'billing_postcode',
+            'billing_country',
+        ]
+        # labels to prettify the form
+        labels = {
+            'shipping_full_name': "Full Name (Shipping)",
+            'billing_full_name': "Full Name (Billing)",
+            # etc...
+        }
+        widgets = {
+            'shipping_full_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'shipping_phone': forms.TextInput(attrs={'class': 'form-control'}),
+            # Add similar widget settings for all fields...
+        }
--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
 # users/views.py
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import CustomUserForm
+from .models import CustomUser
 from orders.models import Order  # Order model is imported
+
+# User profile
 
 @login_required
 def user_profile(request):
@@ -11,3 +17,21 @@ def user_profile(request):
         "user": request.user,
         "orders": orders,
     })
+
+# User profile update
+
+@login_required
+def edit_profile(request):
+    """Allow a logged-in user to edit shipping/billing details."""
+    if request.method == "POST":
+        form = CustomUserForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your profile has been updated!")
+            return redirect("user_profile")
+        else:
+            messages.error(request, "Please correct the errors below.")
+    else:
+        form = CustomUserForm(instance=request.user)
+
+    return render(request, "users/edit_profile.html", {"form": form})
