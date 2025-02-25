812d462c0a2d581f0a0c33fbef0e1222c19ffca8
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2025-02-25 22:16:38 +0000

    Add Edit profile button in profile.html.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import user_profile
+from .views import user_profile, edit_profile
 
 urlpatterns = [
     path("profile/", user_profile, name="user_profile"),
+    path("profile/edit/", edit_profile, name="edit_profile"),
 ]
