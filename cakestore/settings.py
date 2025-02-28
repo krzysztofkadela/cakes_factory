b3da355130f27e1907dd36d2f63e83638540c208
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2025-02-28 22:56:16 +0000

    Feat: Add bsic for newsletter funcyionality.

--- cakestore/settings.py
+++ cakestore/settings.py
@@ -65,6 +65,8 @@ INSTALLED_APPS = [
     'orders',    # Order & payment handling
     'users',     # User authentication
     'home',
+    # Optional-- newsletter function..
+    'newsletter',
 ]
 
 MIDDLEWARE = [
