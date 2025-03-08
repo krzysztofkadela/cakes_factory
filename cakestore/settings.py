a67ef6bcfcc559990db7b111fc6a0ad094c59f3a
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2025-03-08 16:48:05 +0000

    Fixing email verification

--- cakestore/settings.py
+++ cakestore/settings.py
@@ -41,9 +41,9 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'cakefactorystore24@gmail.com')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = 'Cake Factory <cakefactorystore24@gmail.com>'
+    DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Cake Factory <cakefactorystore24@gmail.com>')
 
     # ✅ Secure Email-related settings (production only)
     SECURE_SSL_REDIRECT = True
