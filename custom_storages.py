22b24f292df6d57bc573a71431539c3f0fd0e73f
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2025-03-10 19:46:34 +0000

    Update custom_storages.py

--- custom_storages.py
+++ custom_storages.py
@@ -4,10 +4,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    default_acl = "public-read"  # Ensures static files are publicly accessible
-    querystring_auth = False  # Removes authentication query parameters from URLs
+    default_acl = None  # ✅ Remove ACLs
+    querystring_auth = False  # Prevents authentication query parameters
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-    file_overwrite = False  # Prevents media files from being overwritten
+    default_acl = None  # ✅ Remove ACLs
+    file_overwrite = False  # Prevents overwriting media files
