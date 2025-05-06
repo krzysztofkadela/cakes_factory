d1007df36526bdbed18d45f308e8cbfcc1d3149c
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2025-05-06 19:09:33 +0000

    Fix: use default sitemap domain from Django Sites

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -1,36 +1,31 @@
 from django.contrib.sitemaps import Sitemap
-from django.shortcuts import reverse
-from django.conf import settings
+from django.urls import reverse
 from products.models import Product
-from types import SimpleNamespace
+
 
 class StaticViewSitemap(Sitemap):
+    """Sitemap for static pages"""
     priority = 0.8
     changefreq = "monthly"
 
     def items(self):
         return [
-            "home", 
-            "cart_view", 
-            "checkout", 
-            "order_history", 
-            "payment_success", 
+            "home",
+            "cart_view",
+            "checkout",
+            "order_history",
+            "payment_success",
             "payment_cancel",
-            "user_profile",  
-            "edit_profile"
+            "user_profile",
+            "edit_profile",
         ]
 
     def location(self, item):
         return reverse(item)
 
-    def get_urls(self, site=None, **kwargs):
-        if not site:
-            domain = settings.SITE_URL.replace("https://", "").replace("http://", "").rstrip("/")
-            site = SimpleNamespace(domain=domain, name="Cake Factory")
-        return super().get_urls(site=site, **kwargs)
-
 
 class ProductSitemap(Sitemap):
+    """Sitemap for product pages"""
     priority = 0.9
     changefreq = "weekly"
 
@@ -38,10 +33,4 @@ class ProductSitemap(Sitemap):
         return Product.objects.filter(available=True)
 
     def location(self, item):
-        return f"/products/{item.slug}/"
-
-    def get_urls(self, site=None, **kwargs):
-        if not site:
-            domain = settings.SITE_URL.replace("https://", "").replace("http://", "").rstrip("/")
-            site = SimpleNamespace(domain=domain, name="Cake Factory")
-        return super().get_urls(site=site, **kwargs)
+        return reverse("product_detail", args=[item.pk])
